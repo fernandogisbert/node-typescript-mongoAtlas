bbf46f9d9b639a9d34c5e8d0b3575738f147ec5b
LAPTOP-1U1SBC38\ferna <ferbert15@gmail.com>
2020-12-08 10:05:45 +0100

    *users router updated

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -12,7 +12,7 @@ class UsersRoutes {
         
         this.router.get('/',[checkJwt('Admin') ], usersController.allUsers);
         // En lugar de crear la tabla de ventas he creado esta ruta muestra cada usuario con los libros que ha comprado
-        this.router.get('/:nombre', usersController.userBooks);
+        this.router.get('/:nombre',[checkJwt('User')], usersController.userBooks);
         this.router.post('/', usersController.addUser )
         this.router.delete('/', usersController.deleteUser )
         this.router.put('/', usersController.updateUser)
